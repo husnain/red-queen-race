4586b410ae0ef071f459e2853032fc3dbc7566b2
Husnain Ahmed <husnain.ahmed@skillorbit.com>
2020-12-13 19:28:50 +0500

    red queen race With React hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { RedQueenRace } from "./RedQueenRace";
+
+function App() {
+	return (
+		<div>
+			<RedQueenRace />
+		</div>
+	);
+}
+
+export default App;
